8e11821fa4c4b6b25831a9374fcbd1d87ff5d437
ArmashSiddiqui <157289829+ArmashSiddiqui@users.noreply.github.com>
2024-08-20 15:51:42 +0530

    Update Category.js

--- controller/Category.js
+++ controller/Category.js
@@ -9,15 +9,6 @@ exports.fetchCategories = async (req, res) => {
   }
 };
 
-exports.createCategory = async (req, res) => {
-  const category = new Category(req.body);
-  try {
-    const doc = await category.save();
-    res.status(201).json(doc);
-  } catch (err) {
-    res.status(400).json(err);
-  }
-};
 
 
 
