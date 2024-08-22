4dc07f08357b2282c922dae24ee2a8aa4a43c4b0
ArmashSiddiqui <157289829+ArmashSiddiqui@users.noreply.github.com>
2024-08-22 09:49:41 +0530

    Update SignupPage.js

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,11 +1,2 @@
 import Signup from "../features/auth/components/Signup";
 
-function SignupPage() {
-    return ( 
-        <div>
-            <Signup></Signup>
-        </div>
-     );
-}
-
-export default SignupPage;
